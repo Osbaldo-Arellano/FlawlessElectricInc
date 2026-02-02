73fdef65cf5f8fb33f4c244a56bb48d2538b07d9
Osbaldo-Arellano <osbaldoarellano1996@gmail.com>
2026-02-01 23:07:50 -0800

    remove old refs to print portal

--- components/business-cards/index.ts
+++
@@ -1,4 +0,0 @@
-export { BusinessCardEditor } from "./business-card-editor";
-export { TemplateModern } from "./template-modern";
-export { TemplateBold } from "./template-bold";
-export * from "./types";
